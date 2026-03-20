8594d99934736a2e27824aeb3e0c3d7094e38877
Ashay Patil <ashaypatil2005@gmail.com>
2026-03-20 20:25:15 +0530

    Resolved the error querySrv ECONNREFUSED ,When i try to connect with my Mongo atlas

--- Backend/db/connect.js
+++ Backend/db/connect.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
+// Source - https://stackoverflow.com/a/79874273
+// Posted by Vin
+// Retrieved 2026-03-20, License - CC BY-SA 4.0
+
+require("node:dns/promises").setServers(["1.1.1.1", "8.8.8.8"]);
 
 const connectDB = (url) => {
   return mongoose.connect(url);
